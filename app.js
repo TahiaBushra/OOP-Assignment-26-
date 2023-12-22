634253c0190dfde3bf5a1d1ceffbd42da32fff1c
TahiaBushra <bushratahia@gmail.com>
2023-12-22 23:20:06 +0600

    Classes Done

--- app.js
+++ app.js
@@ -3,14 +3,32 @@
 //OOP -----> Object Oriented Programming
 
 // Constructor Function
-function Person(name, birthYear) {
-    this.name = name;
-    this.birthYear = birthYear;
-    this.calcAge = function () {
-        return new Date().getFullYear - this.birthYear;
-    }
-};
-// instances
-const person1 = new Person("Mimi", 2000);
-console.log(person1.name);
-console.log(person1.calcAge);
+// function Person(name, birthYear) {
+//     this.name = name;
+//     this.birthYear = birthYear;
+//     this.calcAge = function () {
+//         return new Date().getFullYear - this.birthYear;
+//     }
+// };
+// // instances
+// const person1 = new Person("Mimi", 2000);
+// console.log(person1.name);
+// console.log(person1.calcAge);
+
+// Classes
+// class Person {
+//     constructor(name, birthYear) {
+//         this.name = name;
+//         this.birthYear = birthYear;
+//     }
+
+//     calcAge() {
+//         return new Date().getFullYear() - this.birthYear;
+//     }
+// }
+
+// const person1 = new Person("Mimi", 2005);
+// console.log(person1.calcAge());
+
+// const person2 = new Person("Mahi", 2007);
+// console.log(person2.calcAge());
